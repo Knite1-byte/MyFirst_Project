8a26100245bfaa7632751acad7e08905ed0fcd5e
Egor <ivannikovegor3p@email.com>
2025-09-22 00:59:42 +0300

    Удалены все лишние файлы, остался только my_project.py

--- Education/def.py
+++
@@ -1,7 +0,0 @@
-def summa(a, b):
-    return a + b
-
-    
-res = summa(5, 7)
-print(res)
-print(summa("Hi", "i"))
--- Education/module.py
+++
@@ -1,5 +0,0 @@
-import datetime as d, sys, os, platform
-from math import sqrt as s, ceil
-
-# print(d.datetime.now().time())
-print(ceil(s(25)))
